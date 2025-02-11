709dcf9347605261d64e58f78f3ded502054ed2f
Fred - FLQ <strapi.0txni@aleeas.com>
2025-02-11 11:13:20 +0100

    Refactor to OOP approach

--- main.js
+++ main.js
@@ -8,10 +8,97 @@ const pathCharacter = '*';
 class Field {
     constructor(fieldMap) {
         this.fieldMap = fieldMap;
+        this.lineIndex = 0;
+        this.pathIndex = 0;
+        this.initiatePosition();
     }
+
+    // Display fieldMap
     print() {
         for (let i = 0; i < this.fieldMap.length; i++) {
             console.log(this.fieldMap[i].join(''));
+        }
+    }
+
+    // Find pathCharacter in the 2D array
+    initiatePosition() {
+        for (let i = 0; i < this.fieldMap.length; i++) {
+            if (this.fieldMap[i].indexOf(pathCharacter) !== -1) {
+                this.lineIndex = i;
+                this.pathIndex = this.fieldMap[i].indexOf(pathCharacter);
+                break;
+            }
+        }
+    }
+
+    // Move pathCharacter and update position
+    moveCharacter(moveLine, moveIndex) {
+        let inField = true;
+        let notHole = true;
+        let notHat = true;
+
+        // Check field boundaries
+        if (this.lineIndex + moveLine < 0 || this.lineIndex + moveLine >= this.fieldMap.length || this.pathIndex + moveIndex < 0 || this.pathIndex + moveIndex >= this.fieldMap[this.lineIndex].length) {
+            inField = false;
+            return { inField, notHole, notHat };
+        }
+
+        // Move the character
+        this.fieldMap[this.lineIndex][this.pathIndex] = fieldCharacter;
+        this.lineIndex += moveLine;
+        this.pathIndex += moveIndex;
+
+        // Check if hole or hat
+        if (this.fieldMap[this.lineIndex][this.pathIndex] === hole) {
+            notHole = false;
+        } else if (this.fieldMap[this.lineIndex][this.pathIndex] === hat) {
+            notHat = false;
+        }
+
+        // Display character in new position
+        this.fieldMap[this.lineIndex][this.pathIndex] = pathCharacter;
+
+        // Return an object for use in while loop
+        return { inField, notHole, notHat };
+    }
+
+    // Initiate game
+    startGame() {
+        this.print();
+
+        while (true) {
+            let userInput = prompt('Which way do you wanna go?').toLowerCase();
+            let moveResult;
+
+            switch (userInput) {
+                case 'l':
+                    moveResult = this.moveCharacter(0, -1);
+                    break;
+                case 'r':
+                    moveResult = this.moveCharacter(0, 1);
+                    break;
+                case 'u':
+                    moveResult = this.moveCharacter(-1, 0);
+                    break;
+                case 'd':
+                    moveResult = this.moveCharacter(1, 0);
+                    break;
+                default:
+                    console.log('Incorrect input: please use "l", "r", "u" or "d".');
+                    continue;
+            };
+
+            let { inField, notHole, notHat } = moveResult;
+
+            if (!inField) {
+                console.log('You can\'t go that way!');
+            } else if (!notHat) {
+                console.log('You win!');
+                break;
+            } else if (!notHole) {
+                console.log('You felt in a hole! Game over');
+                break;
+            }
         }
     }
 };
@@ -22,87 +109,4 @@ const myField = new Field([
     ['░', '^', '░'],
 ]);
 
-let lineIndex;
-let pathIndex;
-
-// Finding pathCharacter in the 2D array
-const initiatePosition = () => {
-    for (let i = 0; i < myField.fieldMap.length; i++) {
-        if (myField.fieldMap[i].indexOf(pathCharacter) !== -1) {
-            lineIndex = i;
-            pathIndex = myField.fieldMap[i].indexOf(pathCharacter);
-            break;
-        }
-    }
-}
-
-// Moving Character and updating position
-const moveCharacter = (moveLine, moveIndex) => {
-    let inField = true;
-    let notHole = true;
-    let notHat = true;
-
-    // Check field boundaries
-    if (lineIndex + moveLine < 0 || lineIndex + moveLine >= myField.fieldMap.length || pathIndex + moveIndex < 0 || pathIndex + moveIndex >= myField.fieldMap[lineIndex].length) {
-        inField = false;
-        return { inField, notHole, notHat };
-    }
-
-    // Move the character
-    myField.fieldMap[lineIndex][pathIndex] = fieldCharacter;
-    lineIndex += moveLine;
-    pathIndex += moveIndex;
-
-    // Check if hole or hat
-    if (myField.fieldMap[lineIndex][pathIndex] === hole) {
-        notHole = false;
-    } else if (myField.fieldMap[lineIndex][pathIndex] === hat) {
-        notHat = false;
-    }
-
-    // Display character in new position
-    myField.fieldMap[lineIndex][pathIndex] = pathCharacter;
-
-    // Return an object for use in while loop
-    return { inField, notHole, notHat };
-}
-
-initiatePosition();
-myField.print();
-
-while (true) {
-    let userInput = prompt('Which way do you wanna go?').toLowerCase();
-    let moveResult;
-
-    switch (userInput) {
-        case 'l':
-            moveResult = moveCharacter(0, -1);
-            break;
-        case 'r':
-            moveResult = moveCharacter(0, 1);
-            break;
-        case 'u':
-            moveResult = moveCharacter(-1, 0);
-            break;
-        case 'd':
-            moveResult = moveCharacter(1, 0);
-            break;
-        default:
-            console.log('Incorrect input: please use "l", "r", "u" or "d".');
-            continue;
-    };
-
-    myField.print();
-
-    let { inField, notHole, notHat } = moveResult;
-
-    if (!inField) {
-        console.log('You can\'t go that way!');
-    } else if (!notHat) {
-        console.log('You win!');
-        break;
-    } else if (!notHole) {
-        console.log('You felt in a hole! Game over');
-        break;
-    }
-}
+myField.startGame();
